d828e0ef61cb3260146784f76f20d9a9f41193e2
Karthik Vaddempudi <karthikviam@outlook.com>
2024-06-08 09:43:29 -0500

    init

---
+++ src/main/java/com/example/SageIt/SageItApplication.java
@@ -0,0 +1,13 @@
+package com.example.SageIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SageItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SageItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SageIt/SageItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SageIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SageItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
